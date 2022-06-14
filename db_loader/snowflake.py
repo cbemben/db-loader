3325b5a1b07fed29210716d3976700d897b93cfd
cbemben <cbemben04@gmail.com>
2022-06-14 14:04:30 -0400

    adding file handling

--- db_loader/snowflake.py
+++ db_loader/snowflake.py
@@ -6,13 +6,14 @@ def snowflake_connector(func):
     def with_connection_(*args,**kwargs):
         con = snowflake.connector.connect(
         	  	user=os.environ['SNOWFLAKE_USER'],
-        	  	#password=os.environ['SNOWFLAKE_PWD'],
+        	  	password=os.environ['SNOWFLAKE_PWD'],
         	  	account=os.environ['SNOWFLAKE_ACCT'],
         	  	role=os.environ['SNOWFLAKE_ROLE'],
         	  	warehouse=os.environ['SNOWFLAKE_WAREHOUSE'],
         	  	database=os.environ['SNOWFLAKE_DB'],
-        	  	schema=os.environ['SNOWFLAKE_SCHEMA'],
-        	  	authenticator='externalbrowser')
+        	  	schema=os.environ['SNOWFLAKE_SCHEMA']
+        	  	#authenticator='externalbrowser'
+                )
         try:
             rv = func(con, *args,**kwargs)
         except Exception:
@@ -37,3 +38,14 @@ def test_connection(con):
 	cur.execute("SELECT current_version()")
 	ret = cur.fetchone()
 	print(ret)
+
+def get_dir_files(dir_path: str):
+    #return list of files with full path
+
+def put_files_into_snowflake(con, file):
+    #put file into snowflake
+
+@snowflake_connector
+def recurse_over_files(con):
+    #x = get_dir_files
+    #loop over x in put_files_into_snowflake
