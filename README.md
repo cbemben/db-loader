065f12071a20bc9f98bedad8c05e89fef3978aaa
Bemben <christopher.bemben@spectrumhealth.org>
2022-07-21 14:16:18 -0400

    adding docs and platform handling

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # db-loader
-Package to lift external data into db
+Package to lift external data into db.
+
+Currently this process assumes there is a primary directory which contains a set of sub-directories containing the individual files to load. The Directory Path section of the config specifies the primary directory path while the File Paths section specifies the different sub-directories.
+
+## Config Required
+Create a `config.ini` file in the `db_loader` directory with the following structure.
+
+```console
+[DirectoryPath]
+Directory=__path to the directory containing sub-directories which contain individual files__
+
+[FilePaths]
+subdirectory_desc_1=subdirectory_name_1/
+subdirectory_desc_2=subdirectory_name_2/
+subdirectory_desc_3=subdirectory_name_3/
+
+[SnowflakeStage]
+NAMED_STAGE_1=__The name of the Snowflake Named Stage including @ symbol (example: @MY_STAGE)__
+```
+
+## To Run
+Once config file, Snowflake credentials/locations and named stage is in place you can execute any of the functions. The primary runner to iterate and load all files is the `push_to_snowflake_stage()` function.
--- db_loader/snowflake.py
+++ db_loader/snowflake.py
@@ -1,10 +1,14 @@
 import snowflake.connector
-import logging
+import sys
 import os
 import configparser
 from pathlib import Path
 
 def snowflake_connector(func):
+    """ decorator function to pass Snowflake credentials and create
+        connection to database. All necessary connection information
+        must be present as environment variables 
+    """
     def with_connection_(*args,**kwargs):
         con = snowflake.connector.connect(
         	  	user=os.environ['SNOWFLAKE_USER'],
@@ -31,18 +35,25 @@ def snowflake_connector(func):
     return with_connection_
 
 def get_config_value(section: str, key: str, config_path: str = 'config.ini') -> str:
+    """ Helper function to help collect and parse config values.
+    """
     config = configparser.ConfigParser()
     config.read(config_path)
     return config[section][key]
 
 def get_list_of_files():
+    """ Returns a list of individual filenames that are present in the target directory
+    """
     dir_path = get_config_value(section="DirectoryPath",key="Directory")
-    dir_name = get_config_value(section="FilePaths", key="RadiologyEpisodes")
+    dir_name = get_config_value(section="FilePaths", key="MedicalEpisodes")
     full_path = dir_path + dir_name
     list_of_files = list(Path(full_path).glob('*.*'))
     return list_of_files
 
 def get_diff_list_of_files():
+    """ Returns a list of files that have NOT been loaded into the target
+        Snowflake named stage defined in the config file
+    """
     incoming_files = [str(i) for i in get_list_of_files()]
     files_in_snowflake = get_list_of_files_in_stage()
     z = []
@@ -55,6 +66,9 @@ def get_diff_list_of_files():
 
 @snowflake_connector
 def get_list_of_files_in_stage(con):
+    """ Returns a list of files in the Snowflake named stage defined
+        in the config file.
+    """
     snowflake_stage_name = get_config_value("SnowflakeStage","NAMED_STAGE_1")
     query = 'select distinct metadata$filename from ' + snowflake_stage_name
     print(query)
@@ -64,9 +78,14 @@ def get_list_of_files_in_stage(con):
 
 @snowflake_connector
 def push_to_snowflake_stage(con, snowflake_stage_name: str = None):
+    """ Moves files from the target directory into the Snowflake named stage.
+    """
     list_of_files = get_diff_list_of_files()
     snowflake_stage_name = get_config_value("SnowflakeStage","NAMED_STAGE_1")
     for i in list_of_files:
-        x = "PUT 'file://" + i + "' " + snowflake_stage_name
+        if sys.platform is 'win32':
+            x = "PUT 'file://" + Path(i).as_posix() + "' " + snowflake_stage_name
+        else:
+            x = "PUT 'file://" + i + "' " + snowflake_stage_name
         print(x)
         con.cursor().execute(x)
